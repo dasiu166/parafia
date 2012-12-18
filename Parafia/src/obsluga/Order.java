f2faeca56873a3a2605e1bb89d9181b9dfdbf72e
dasiu166 <dasiu166@wp.pl>
2012-12-18 21:52:09 +0100

    Lekka zmiana w zamowieniu, dodane pole status oznaczajace probe rezerwacji/rezerwacje zamowienia

--- Parafia/src/obsluga/Order.java
+++ Parafia/src/obsluga/Order.java
@@ -9,11 +9,17 @@ public class Order extends DataBaseElement implements Serializable{
 	private String senderPesel; //pesel tego co nadal zamowienie
 	
 	private Parishioner executor;//ten co ma odprawic zamowienie
-	private String executorPesel;//pesel tego co maodprawic zamowienie
+	private String executorPesel;//pesel tego co ma odprawic zamowienie
 	
 	/*UP - narazie nie wiem ktore bedzie obowiazujace, zapewnie niedlugo
 	 * bedzie tak ze w lekkim zamowieniu beda pesele i daty
 	 * a w ciezkim cale informacje o nadawcy/odbiorcy/tresci itp*/
+	
+	private String status;/*oznacza probe_zarezerewowania terminu lub
+	ze termin zarezerwowany, nie bedzie tu odrzucenia zamowienia bo wtedy
+	odrazu pojdzie zapytania kasujace taki rekord  z bazy*/
+	
+	
 	
 	private String event;//zawiera rodzaj zamowienia (msza/wizyta itp)
 	private String describe;//opis wydarzenie
@@ -38,6 +44,9 @@ public class Order extends DataBaseElement implements Serializable{
 	public void setEndDate(Date d){
 		endD = d;
 	}
+	public void setStatus(String val){
+		status = val;
+	}
 
 	public String getSenderPesel(){
 		return senderPesel;
@@ -56,6 +65,9 @@ public class Order extends DataBaseElement implements Serializable{
 	}
 	public Date getEndDate(){
 		return endD;
+	}
+	public String getStatus(){
+		return status;
 	}
 	
 	public boolean setQuery(String val){
